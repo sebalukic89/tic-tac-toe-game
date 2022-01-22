dc35cf4e333cb73baecc97082c6b0702da952aae
Szebasztian Lukity <lukicseba@gmail.com>
2022-01-22 11:46:34 -0500

    added the final touches to the logic when the game ends

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,33 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML =
+    'You won, <span id="winner-name">PLAYER NAME</span>';
+  gameOverElement.style.display = 'none';
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = '';
+      gameBoardItemElement.classList.remove('disabled');
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
   if (players[0].name === '' || players[1].name === '') {
     alert('Please set custom player names for both players!');
     return;
-  } else {
-    activePlayerNameElement.textContent = players[activePlayer].name;
-    gameAreaElement.style.display = 'block';
   }
+
+  resetGameStatus();
+
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = 'block';
 }
 
 function switchPlayer() {
@@ -20,7 +42,7 @@ function switchPlayer() {
 function selectGameField(event) {
   const selectedField = event.target;
 
-  if (selectedField.tagName !== 'LI') {
+  if (selectedField.tagName !== 'LI' || gameIsOver) {
     return;
   }
 
@@ -38,7 +60,10 @@ function selectGameField(event) {
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
   const winnerId = checkForGameOver();
-  console.log(winnerId);
+
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
 
   currentRound++;
   switchPlayer();
@@ -88,4 +113,17 @@ function checkForGameOver() {
     return -1;
   }
   return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = 'block';
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "It's a draw!";
+  }
 }
